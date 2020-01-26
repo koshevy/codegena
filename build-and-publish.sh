9da6c92e9d7b720e2173f8059b3f39f8929e3506
Alexander Koshevy <koshevy@gmail.com>
2020-01-26 10:18:46 +0300

    chore: update lerna publisher and related configuration in packages

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -7,4 +7,4 @@ yarn run build:all-libs &&
 yarn run test:libs &&
 rm -rf dist/* && # second clearing before it builds changed libs
 ./node_modules/.bin/lerna run build --since $(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1)) &&
-./node_modules/.bin/lerna publish --contents dist
+./node_modules/.bin/lerna publish --yes --contents dist
