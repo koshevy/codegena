77a8c937b2f652a1b7cda128c1c60a851cae43dd
Alexander Koshevy <koshevy@gmail.com>
2020-02-03 10:53:15 +0300

    chore: specify base axios config

--- libs/axios-wrapper/src/lib/configs/default-config.ts
+++ libs/axios-wrapper/src/lib/configs/default-config.ts
@@ -1,3 +1,11 @@
-export const defaultAxiosConfig = {
+import { AxiosRequestConfig } from 'axios';
 
+export const defaultAxiosConfig: AxiosRequestConfig = {
+    headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+    },
+    responseType: 'json',
+    timeout: 60000,
+    withCredentials: true
 };
