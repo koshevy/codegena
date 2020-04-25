fe9198b4fd8a2db67764eb47ba8e0b116674650d
Alexander Koshevy <koshevy@gmail.com>
2020-04-25 17:39:59 +0300

    chore: update git flow

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
 
 branch="$(git symbolic-ref --short -q HEAD)"
+releaseMode=''
 
-if [ "$branch" != "master" ]; then
-    echo -e "\033[0;31m";
-    echo "╔════════════════════════════════════════════════╗"
-    echo "║ Publication MUST be run only in master branch! ║"
-    echo "║                                                ║"
-    echo "║ Please, make Pull Request in master branch and ║"
-    echo "║ continue publishing after PR merging.          ║"
-    echo "╚════════════════════════════════════════════════╝"
-    echo -e "\033[0m";
-    exit 1;
+if [ "$branch" == "master" ]
+then
+    echo "Sterting pre-publishing..."
+    releaseMode="--conventional-prerelease"
+else
+    if ["$branch" == "release"]
+    then
+        releaseMode="--conventional-graduate --no-push"
+        echo "Sterting publishing..."
+    else
+        echo -e "\033[0;31m";
+        echo "╔════════════════════════════════════════════════╗"
+        echo "║ Publication MUST be run only in master branch  ║"
+        echo "║ or in release branch!                          ║"
+        echo "║                                                ║"
+        echo "║ Please, make Pull Request in master branch and ║"
+        echo "║ continue publishing after PR merging.          ║"
+        echo "╚════════════════════════════════════════════════╝"
+        echo -e "\033[0m";
+        exit 1;
+    fi
 fi
 
-yarn lint &&
 rm -rf dist/* &&
+yarn run test:libs &&
 # version update error ignoring because
 # there may be unpublished packages with updated version
-(./node_modules/.bin/lerna version --conventional-commits --yes --allow-branch master || true) &&
+(sh -c "./node_modules/.bin/lerna version --amend --conventional-commits --yes --no-push --no-git-tag-version $releaseMode" || true) &&
 yarn run build:all-libs &&
 yarn run test:libs &&
 
