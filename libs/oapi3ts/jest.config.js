df6d5d0a08724fbff6d22ac6202bd1f00f9e7ec1
Alexander Koshevy <koshevy@gmail.com>
2020-10-25 12:26:45 +0300

    chore(oapi3ts): setup oapi3ts library settings

--- libs/oapi3ts/jest.config.js
+++ libs/oapi3ts/jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   name: 'oapi3ts',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/oapi3ts'
+  coverageDirectory: '../../coverage/libs/oapi3ts',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
 };
